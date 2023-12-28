4a6658aa3715168cb16139e33f8b54ba481894e6
Dima <dimafax222@gmail.com>
2023-12-28 14:54:29 +0200

    first commit

---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,23 @@
+import EmployeesListItem from "../employees-list-item/employees-list-item";
+
+import './employees-list.css';
+
+const EmployeesList = ({data}) => {
+
+const elements = data.map(item => {
+
+    const {id, ...ItemProps} = item;
+
+    return (
+        <EmployeesListItem key={id} {...ItemProps}/>
+)
+})
+
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    )
+}
+
+export default EmployeesList;
